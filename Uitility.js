b6da048d96ce9b8cc52d215c115d4fc01b04d2b6
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2024-08-19 22:02:11 +0600

    add js for booking seat and price calculations

---
+++ Uitility.js
@@ -0,0 +1,24 @@
+// function priceCalculation(price) {
+//   let ticketPrice = 550;
+//   let totalPrice = price + ticketPrice;
+//   console.log(totalPrice);
+//   return totalPrice;
+// }
+
+//coupon discount
+// let couponBtn = document.getElementById("apply-btn");
+// couponBtn.addEventListener("click", function couponDis(totalPrice) {
+//   let input = document.getElementById("coupon-input");
+//   let GrandTotal = document.getElementById("grand-total");
+//   if (input.value === "New15") {
+//     let discount = (totalPrice * 15) / 100;
+//     let NetPrice = totalPrice - discount;
+//     GrandTotal.innerText = NetPrice;
+//   } else if (input.value === "Couple20") {
+//     let discount = (totalPrice * 20) / 100;
+//     let NetPrice = totalPrice - discount;
+//     GrandTotal.innerText = NetPrice;
+//   } else {
+//     alert("Wrong Coupon Code!");
+//   }
+// });
--- script.js
+++ script.js
@@ -5,22 +5,64 @@ let seat = document.getElementById("aviableSeatCount");
 let SelectedSeat = document.getElementById("Selected-seat");
 let billSection = document.getElementById("bill-section");
 let BDT = document.getElementById("total-price");
+let noSeat = document.getElementById("no-seat");
 
+const seats_obj = {
+  A1: false,
+  A2: false,
+  A3: false,
+  A4: false,
+  B1: false,
+  B2: false,
+  B3: false,
+  B4: false,
+  C1: false,
+  C2: false,
+  C3: false,
+  C4: false,
+  D1: false,
+  D2: false,
+  D3: false,
+  D4: false,
+  E1: false,
+  E2: false,
+  E3: false,
+  E4: false,
+  F1: false,
+  F2: false,
+  F3: false,
+  F4: false,
+  G1: false,
+  G2: false,
+  G3: false,
+  G4: false,
+  H1: false,
+  H2: false,
+  H3: false,
+  H4: false,
+  I1: false,
+  I2: false,
+  I3: false,
+  I4: false,
+  J1: false,
+  J2: false,
+  J3: false,
+  J4: false,
+}
 function selectSeat(id) {
   let doc = document.getElementById(id);
-  if (state == 0) {
+  if (seats_obj[id] === false) {
     doc.style.backgroundColor = "#1dd100";
     doc.style.color = "#fff";
-    let aviableSeat = parseInt(seat.innerText) - 1;
+    let availableSeat = parseInt(seat.innerText) - 1;
     let newSelectedSeat = parseInt(SelectedSeat.innerText) + 1;
-    seat.innerText = aviableSeat;
+    seat.innerText = availableSeat;
     SelectedSeat.innerText = newSelectedSeat;
 
     let setNumber = document.getElementById(id);
-    let noSeat = document.getElementById("no-seat");
-    noSeat.hidden = true;
     let div = document.createElement("div");
     div.classList.add("grey-text", "align");
+    div.setAttribute("id", id + "selected_seat");
     let seatNumP = document.createElement("p");
     let seatClassP = document.createElement("p");
     let seatPriceP = document.createElement("p");
@@ -36,13 +78,26 @@ function selectSeat(id) {
     let ticketPrice = 550;
     let totalPrice = parseInt(BDT.innerText) + ticketPrice;
     BDT.innerText = totalPrice;
-    console.log(totalPrice);
 
-    state = 1;
+    seats_obj[id] = true;
     seatCount++;
-    console.log(seat.innerText, seatCount);
   } else {
     doc.style.backgroundColor = "#fff";
-    state = 0;
+    doc.style.color = "#737373";
+
+    let availableSeat = parseInt(seat.innerText) + 1;
+    let newSelectedSeat = parseInt(SelectedSeat.innerText) - 1;
+    seat.innerText = availableSeat;
+    SelectedSeat.innerText = newSelectedSeat;
+    let removedChild = document.getElementById(id + "selected_seat");
+    billSection.removeChild(removedChild);
+
+    let ticketPrice = 550;
+    let totalPrice = parseInt(BDT.innerText) - ticketPrice;
+    BDT.innerText = totalPrice;
+
+    seats_obj[id] = false;
+    seatCount--;
   }
+  console.log(seatCount);
 }
